2580eabe0027831ac97c8f597a9a19d420bcc424
dim171 <styler1715@gmail.com>
2021-10-18 12:57:30 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let category = 'toys';
+console.log(`https://someurl.com/${category}`)
+
+console.log(4 + +'5');
+
+let incr = 10,
+   decr = 10;
+
+   // ++incr;
+   // --decr;
+   console.log(++incr);
+   console.log(--decr);
+
+   console.log(2 + 2 * 2 != '8');
+
+   const isChecked = false,
+      isClose = false;
+      console.log(isChecked || !isClose);
+
